bf1d7ae0be7f7936aaed7699f08e5c5cd2829910
Bayazid Bustami Surid <20230557029@juniv.edu>
2026-04-23 00:59:45 +0600

    Update axiosInstance.js
    ;

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const VITE_API_BASE_URL='https://somagoam-database-dlilb.ondigitalocean.app/backend'
+const VITE_API_BASE_URL='https://somagoam-database-dlilb.ondigitalocean.app/backend';
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
